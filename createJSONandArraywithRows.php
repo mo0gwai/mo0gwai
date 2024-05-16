4c4ea57c177e9d6e0bd82d95214d6ee9d622dd79
Fuegedi G <gyula.fuegedi@becare.solutions>
2024-05-16 17:44:11 +0200

    sad
    asdasd

---
+++ createJSONandArraywithRows.php
@@ -0,0 +1,283 @@
+<pre>Array
+(
+    [Field] => Array
+        (
+            [0] => Array
+                (
+                    [FieldName] => KONTIERUNG
+                    [ReadOnly] => 
+                    [SystemField] => 
+                    [ItemElementName] => Table
+                    [PointAndShootInfo] => 
+                    [IsAutoNumber] => 
+                    [IsNull] => 
+                    [Item] => Array
+                        (
+                            [$type] => DocumentIndexFieldTable
+                            [Row] => Array
+                                (
+                                    [0] => Array
+                                        (
+                                            [ColumnValue] => Array
+                                                (
+                                                    [0] => Array
+                                                        (
+                                                            [FieldName] => KONTI_NETTOBETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 23,12
+                                                            [FieldValue] => 23,12
+                                                        )
+
+                                                    [1] => Array
+                                                        (
+                                                            [FieldName] => KONTI_UST_SCHLUSSEL
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => String
+                                                            [PointAndShootInfo] => 
+                                                            [Item] => V0 - ALLE Kein Steuervorgang 24 
+                                                            [FieldValue] => V0 - ALLE Kein Steuervorgang 24
+                                                        )
+
+                                                    [2] => Array
+                                                        (
+                                                            [FieldName] => KONTI_UST_BETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                    [3] => Array
+                                                        (
+                                                            [FieldName] => KONTI_BRUTTO_BETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => be.care Test aus JR Aufruf 23
+                                                            [FieldValue] => be.care Test aus JR Aufruf 23
+                                                        )
+
+                                                    [4] => Array
+                                                        (
+                                                            [FieldName] => KONTI_LEISTUNGSBEZUGSDATUM
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => DateTime
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                    [5] => Array
+                                                        (
+                                                            [FieldName] => KONTI_BUGDETPOSITIONSTYP
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => String
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                )
+
+                                        )
+
+                                    [1] => Array
+                                        (
+                                            [ColumnValue] => Array
+                                                (
+                                                    [0] => Array
+                                                        (
+                                                            [FieldName] => KONTI_NETTOBETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 23,12
+                                                            [FieldValue] => 23,12
+                                                        )
+
+                                                    [1] => Array
+                                                        (
+                                                            [FieldName] => KONTI_UST_SCHLUSSEL
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => String
+                                                            [PointAndShootInfo] => 
+                                                            [Item] => V0 - ALLE Kein Steuervorgang 24 
+                                                            [FieldValue] => V0 - ALLE Kein Steuervorgang 24
+                                                        )
+
+                                                    [2] => Array
+                                                        (
+                                                            [FieldName] => KONTI_UST_BETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                    [3] => Array
+                                                        (
+                                                            [FieldName] => KONTI_BRUTTO_BETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => be.care Test aus JR Aufruf 23
+                                                            [FieldValue] => be.care Test aus JR Aufruf 23
+                                                        )
+
+                                                    [4] => Array
+                                                        (
+                                                            [FieldName] => KONTI_LEISTUNGSBEZUGSDATUM
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => DateTime
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                    [5] => Array
+                                                        (
+                                                            [FieldName] => KONTI_BUGDETPOSITIONSTYP
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => String
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                )
+
+                                        )
+
+                                    [2] => Array
+                                        (
+                                            [ColumnValue] => Array
+                                                (
+                                                    [0] => Array
+                                                        (
+                                                            [FieldName] => KONTI_NETTOBETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 23,12
+                                                            [FieldValue] => 23,12
+                                                        )
+
+                                                    [1] => Array
+                                                        (
+                                                            [FieldName] => KONTI_UST_SCHLUSSEL
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => String
+                                                            [PointAndShootInfo] => 
+                                                            [Item] => V0 - ALLE Kein Steuervorgang 24 
+                                                            [FieldValue] => V0 - ALLE Kein Steuervorgang 24
+                                                        )
+
+                                                    [2] => Array
+                                                        (
+                                                            [FieldName] => KONTI_UST_BETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                    [3] => Array
+                                                        (
+                                                            [FieldName] => KONTI_BRUTTO_BETRAG
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => Decimal
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => be.care Test aus JR Aufruf 23
+                                                            [FieldValue] => be.care Test aus JR Aufruf 23
+                                                        )
+
+                                                    [4] => Array
+                                                        (
+                                                            [FieldName] => KONTI_LEISTUNGSBEZUGSDATUM
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => DateTime
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                    [5] => Array
+                                                        (
+                                                            [FieldName] => KONTI_BUGDETPOSITIONSTYP
+                                                            [ReadOnly] => 
+                                                            [SystemField] => 
+                                                            [ItemElementName] => String
+                                                            [PointAndShootInfo] => 
+                                                            [IsAutoNumber] => 
+                                                            [IsNull] => 
+                                                            [Item] => 004934556601042 25
+                                                            [FieldValue] => 004934556601042 25
+                                                        )
+
+                                                )
+
+                                        )
+
+                                )
+
+                        )
+
+                )
+
+        )
+
+    [DialogId] => i_dc71012a-ecc8-4cde-8641-d13bb54b0b64
+    [NormalizeCoordinates] => 1
+)
+</pre>
+
